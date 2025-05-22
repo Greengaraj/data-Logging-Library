c31706801fc6ab086dd5c158e3527834a67590d0
Grigory Dvornikov <gringaraj@gmail.com>
2025-05-22 09:37:39 +0300

    add first version

---
+++ dataLoggingLibrary/dataLoggingLibrary/logger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace dataLoggingLibrary {
+    public class logger {
+        public static Action<string> UTLogCallback;
+        public static readonly string logFilePath = "app.log", fildSaveLog = "error.log";
+
+        public static void Log(string message, string level = "INFO") {
+            string timeStamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            string userName = Environment.UserName;
+            string formated = $"{timeStamp}\t[{level}]\t\t{userName}\t\t{message}";
+
+            try {
+                File.AppendAllText(logFilePath, formated + Environment.NewLine);
+            } catch (Exception ex) {
+                message = DateTime.Now.ToString() + "\t" + ex.Message + "\t" + ex.TargetSite + "\t" + ex.StackTrace;
+                File.AppendAllText(fildSaveLog, message + Environment.NewLine);
+            }
+        }
+
+        public static void Info(string message) => Log(message, "INFO");
+        public static void Error(string message) => Log(message, "ERROR");
+        public static void Warn(string message) => Log(message, "WARN");
+    }
+}
